8bd8f82630d40def1ed03a31a07941f605e9cacb
Gertz <clevino512@gmail.com>
2025-09-20 21:18:34 +0300

    Version 5.0.0

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:8000/api";
-// export const API_BASE_URL = "http://redtsingy.msi2025a.net/api";
+// export const API_BASE_URL = "http://localhost:8000/api";
+export const API_BASE_URL = "http://redtsingy.msi2025a.net/api";
